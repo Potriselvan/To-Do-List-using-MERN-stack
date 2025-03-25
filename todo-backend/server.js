82a90422767196305100eabc05afa4f2b67b98ca
Potriselvan <potriskill@gmail.com>
2025-03-25 20:01:51 +0530

    express module is called using app where get and listen methods are defined with port as 3000

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,1 +1,16 @@
 //Express
+const express = require('express');
+
+//create an instance of express
+const app = express();
+
+//Define a route
+app.get('/',(req, res) => {
+    res.send("Hello World!")
+})
+
+//Start the server
+const port = 3000;
+app.listen(port, () => {
+    console.log("Server is listening to port"+port)
+})
