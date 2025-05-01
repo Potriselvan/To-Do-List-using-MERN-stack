a420f5368610e2871fa786f70378dec5ef2fc472
Potriselvan <potriskill@gmail.com>
2025-05-02 02:19:09 +0530

    Creating a new item API

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -4,9 +4,20 @@ const express = require('express');
 //create an instance of express
 const app = express();
 
-//Define a route
-app.get('/',(req, res) => {
-    res.send("Hello World!")
+//Sample in-memory storage for todo items
+let todos = [];
+
+//Create a new todo item
+app.poast('/todos', (req, res) => {
+    const {title, description} = req.body;
+    const newTodo = {
+        id: todos.length + 1,
+        title,
+        description
+    };
+    todos.push(newTodo);
+    console.log(todos);
+    res.status(201).json(newTodo);
 })
 
 //Start the server
