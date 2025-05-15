eaecc3ea6d72e9240ed61f2b95a0b62e39e6c243
Potriselvan <potriskill@gmail.com>
2025-05-16 04:27:05 +0530

    Made both title and description to get input form value

--- todo-frontend/src/Todo.js
+++ todo-frontend/src/Todo.js
@@ -1,4 +1,14 @@
+import { useState } from "react"
+
 export default function Todo() {
+    
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+    const handleSubmit = () => {
+        //check inputs
+    }
+
     return <>
         <div className="row p-3 bg-success text-light">
             <h1>To Do Project with MERN stack</h1>
@@ -7,9 +17,9 @@ export default function Todo() {
             <h3>Add Item</h3>
             <p className="text-success">Item added successfully</p>
             <div className="form-group d-flex gap-2">
-                <input placeholder="Title" className="form-control" type="text" />
-                <input placeholder="Description" className="form-control" type="text" />
-                <button className="btn btn-dark">Submit</button>
+                <input placeholder="Title" onChange={(e) => setTitle(e.target.value)} value={title} className="form-control" type="text" />
+                <input placeholder="Description" onChange={(e) => setDescription(e.target.value)} value={description} className="form-control" type="text" />
+                <button className="btn btn-dark" onClick={handleSubmit}>Submit</button>
             </div>
         </div>
     </>
