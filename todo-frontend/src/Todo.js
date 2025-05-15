85ff2d4a43bffb3e567198167c93f869dabba291
Potriselvan <potriskill@gmail.com>
2025-05-16 04:10:18 +0530

    Creating item and form group is created

--- todo-frontend/src/Todo.js
+++ todo-frontend/src/Todo.js
@@ -3,8 +3,14 @@ export default function Todo() {
         <div className="row p-3 bg-success text-light">
             <h1>ToDo Project with MERN stack</h1>
         </div>
-        <div>
-            
+        <div className="row">
+            <h3>Add Item</h3>
+            <p className="text-success">Item added successfully</p>
+            <div className="form-group d-flex gap-2">
+                <input placeholder="Title" className="form-control" type="text" />
+                <input placeholder="Description" className="form-control" type="text" />
+                <button className="btn btn-dark">Submit</button>
+            </div>
         </div>
     </>
 }
